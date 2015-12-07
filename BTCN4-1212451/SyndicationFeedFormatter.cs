f8a14b0903db9671f6a177ff2af691d1aeb122df
tijikun7764119x <thanhtrungly912@gmail.com>
2015-12-07 15:49:41 +0700

    a

--- BTCN4-1212451/SyndicationFeedFormatter.cs
+++ BTCN4-1212451/SyndicationFeedFormatter.cs
@@ -56,7 +56,7 @@ namespace BTCN4_1212451
             List<SyndicationItem> items = new List<SyndicationItem>();
             var feed = new SyndicationFeed()
             {
-                Title = new TextSyndicationContent("My Feed")
+                Title = new TextSyndicationContent("Luong Thanh Trung - 1212451")
             };
 
             if (models is IEnumerable<News>)
@@ -98,10 +98,10 @@ namespace BTCN4_1212451
             {
                 Title = new TextSyndicationContent(u.title),
                 Content = new TextSyndicationContent(u.title),
-                BaseUri = new Uri(u.href),
+               // BaseUri = new Uri(u.href),
                 PublishDate = new DateTimeOffset(dt)
             };
-            item.Authors.Add(new SyndicationPerson() { Name = "1212451-FITHCMUS" });
+            item.Authors.Add(new SyndicationPerson() { Name = "FITHCMUS" });
             return item;
         }
     }
